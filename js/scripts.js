de650c602f55fd65a983d8edcecc686ba3c061c4
Jordan Blakey <jordan@jordanblakey.com>
2017-01-17 13:19:48 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
-/*
+
 // Load all sections via AJAX
 $(document).ready(function(){
-    $('.lesson1').load('/lessons/lesson1.html');
-    $('.lesson2').load('/lessons/lesson2.html');
-    $('.lesson3').load('/lessons/lesson3.html');
-    $('.lesson4').load('/lessons/lesson4.html');
-    $('.lesson5').load('/lessons/lesson5.html');
+    $('.lesson1').load('./lessons/lesson1.html');
+    $('.lesson2').load('./lessons/lesson2.html');
+    $('.lesson3').load('./lessons/lesson3.html');
+    $('.lesson4').load('./lessons/lesson4.html');
+    $('.lesson5').load('./lessons/lesson5.html');
 });
 
 // Require all scripts via jQuery
 $(document).ready(function(){
     var index = 1;
-    $.getScript('js/lessons1And2.js', function(){console.log('Lessons '+index+' & '+(index += 1)+' scripts loaded.'); index++});
-    $.getScript('js/lesson3.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
-    $.getScript('js/lesson4.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
-    $.getScript('js/lesson5.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+    $.getScript('./js/lessons1And2.js', function(){console.log('Lessons '+index+' & '+(index += 1)+' scripts loaded.'); index++});
+    $.getScript('./js/lesson3.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+    $.getScript('./js/lesson4.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+    $.getScript('./js/lesson5.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
 });
-*/
 
+
+/*
 // Load all sections via AJAX
 $(document).ready(function(){
     $('.lesson1').load('jquery-in-one-page/lessons/lesson1.html');
@@ -35,3 +36,4 @@ $(document).ready(function(){
     $.getScript('jquery-in-one-page/js/lesson4.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
     $.getScript('jquery-in-one-page/js/lesson5.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
 });
+*/
