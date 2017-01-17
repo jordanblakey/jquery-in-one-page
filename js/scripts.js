bd6f4af9c3c9f98881aee11a9378f31b522d1df4
Jordan Blakey <jordan@jordanblakey.com>
2017-01-17 13:18:01 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/*
 // Load all sections via AJAX
 $(document).ready(function(){
     $('.lesson1').load('/lessons/lesson1.html');
@@ -14,4 +15,23 @@ $(document).ready(function(){
     $.getScript('js/lesson3.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
     $.getScript('js/lesson4.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
     $.getScript('js/lesson5.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+});
+*/
+
+// Load all sections via AJAX
+$(document).ready(function(){
+    $('.lesson1').load('jquery-in-one-page/lessons/lesson1.html');
+    $('.lesson2').load('jquery-in-one-page/lessons/lesson2.html');
+    $('.lesson3').load('jquery-in-one-page/lessons/lesson3.html');
+    $('.lesson4').load('jquery-in-one-page/lessons/lesson4.html');
+    $('.lesson5').load('jquery-in-one-page/lessons/lesson5.html');
+});
+
+// Require all scripts via jQuery
+$(document).ready(function(){
+    var index = 1;
+    $.getScript('jquery-in-one-page/js/lessons1And2.js', function(){console.log('Lessons '+index+' & '+(index += 1)+' scripts loaded.'); index++});
+    $.getScript('jquery-in-one-page/js/lesson3.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+    $.getScript('jquery-in-one-page/js/lesson4.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
+    $.getScript('jquery-in-one-page/js/lesson5.js', function(){console.log('Lesson '+index+' script loaded.'); index++;});
 });
